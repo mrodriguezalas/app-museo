6d9c9db08f416091427c414c832a82825732b58d
mrodriguezalas <mauricio.rodriguez.alas@gmail.com>
2020-11-09 21:12:52 -0600

    Fixed running issues with project

---
+++ app_mamiferos/lib/animal_data.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+class AnimalData{
+  static final getData = [
+    {
+      'id' : 15,
+      'nombre' : 'Manigordo - ocelote',
+      'nombreCientifico' : 'Leopardus pardalis',
+      'imagen' : 'assets/images/15_Leopardus pardalis TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/15_Huellas Escaneos14 Leopardus pardalis - manigordo - ocelote.webp'
+    },
+    {
+      'id' : 16,
+      'nombre' : 'Tigrillo - caucel - margay',
+      'nombreCientifico' : 'Leopardus wiedii',
+      'imagen' : 'assets/images/16_Leopardus wiedii TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/16_Huellas Escaneos16 Leopardus wiedii - tigrillo - caucel - margay.webp'
+    },
+    {
+      'id' : 17,
+      'nombre' : 'Tigrillo',
+      'nombreCientifico' : 'Leopardus tigrinus',
+      'imagen' : 'assets/images/17_Leopardus tigrinus TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/17_Huellas Escaneos15 Leopardus tigrinus.webp'
+    },
+    {
+      'id' : 18,
+      'nombre' : 'León breñero - yaguarundi',
+      'nombreCientifico' : 'Puma yagouaroundi',
+      'imagen' : 'assets/images/18_Puma yagouaroundi TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/18_Huellas Escaneos29 Puma yagouaroundi.webp'
+    },
+    {
+      'id' : 19,
+      'nombre' : 'Puma - león americano',
+      'nombreCientifico' : 'Puma concolor',
+      'imagen' : 'assets/images/19_Puma concolor TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/19_Huellas Escaneos28 Puma concolor - puma.webp'
+    },
+    {
+      'id' : 20,
+      'nombre' : 'Jaguar',
+      'nombreCientifico' : 'Panthera onca',
+      'imagen' : 'assets/images/20_Panthera onca TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/20_Huellas Escaneos23 Panthera onca - jaguar.webp'
+    },
+    {
+      'id' : 21,
+      'nombre' : 'Mapache - Oso lavador',
+      'nombreCientifico' : 'Procycon lotor',
+      'imagen' : 'assets/images/21_Procycon lotor TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/21_Huellas Escaneos27 Procyon lotor - mapachin - osito lavador.webp'
+    },
+    {
+      'id' : 22,
+      'nombre' : 'Pizote',
+      'nombreCientifico' : 'Nazua narica',
+      'imagen' : 'assets/images/22_Nazua narica TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/22_Huellas Escaneos21 Nasua narica - pizote - pizote solo - coati.webp'
+    },
+    {
+      'id' : 23,
+      'nombre' : 'Martilla',
+      'nombreCientifico' : 'Potos flavus',
+      'imagen' : 'assets/images/23_Potos flavus TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/23_Huellas Escaneos26 Potos flavus - martilla.webp'
+    },
+    {
+      'id' : 24,
+      'nombre' : 'Cacomistle',
+      'nombreCientifico' : 'Eira barbara',
+      'imagen' : 'assets/images/24_Bassariscus sumichrasti TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/24_Huellas Escaneos01 Bassariscus sumichrasti - cacomistle - ostoche.webp'
+    },
+    {
+      'id' : 25,
+      'nombre' : 'Comadreja',
+      'nombreCientifico' : 'Mustela frenata',
+      'imagen' : 'assets/images/25_Mustela frenata TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/25_Huellas Escaneos19 Mustela frenata - comadreja.webp'
+    },
+    {
+      'id' : 26,
+      'nombre' : 'Grisón - hurón',
+      'nombreCientifico' : 'Galictis vittata',
+      'imagen' : 'assets/images/26_Galictis vittata TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/26_Huellas Escaneos13 Galictis vittata - grisón - hurón.webp'
+    },
+    {
+      'id' : 27,
+      'nombre' : 'Tolomuco',
+      'nombreCientifico' : 'Eira barbara',
+      'imagen' : 'assets/images/27_Eira barbara TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/27_Huellas Escaneos12 Eira barbara - tolomuco - cholomuco.webp'
+    },
+    {
+      'id' : 28,
+      'nombre' : 'Zorro hediondo',
+      'nombreCientifico' : 'Conepatus semistriatus',
+      'imagen' : 'assets/images/28_Conepatus semistriatus TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/28_Huellas Escaneos06 Conepatus semistriatus - zorro hediondo.webp'
+    },
+    {
+      'id' : 29,
+      'nombre' : 'Nutria - Perro de agua',
+      'nombreCientifico' : 'Lontra longicaudis',
+      'imagen' : 'assets/images/29_Lontra longicaudis TDL Lisa Sanchez Aguilar.webp',
+      'huella' : 'assets/images/29_Huellas Escaneos17 Lontra longicaudis - nutria - perro de agua.webp'
+    },
+    {
+      'id' : 30,
+      'nombre' : 'Danta - Macho de Monte',
+      'nombreCientifico' : 'Tapirus bairdii',
+      'imagen' : 'assets/images/30_Tapirusbairdii.webp',
+      'huella' : 'assets/images/30_Tapirusbairdiidantadantotapirmachodemonte.webp'
+    },
+    {
+      'id' : 31,
+      'nombre' : 'Saino',
+      'nombreCientifico' : 'Pecaritajacu',
+      'imagen' : 'assets/images/31_Pecaritajacu.webp',
+      'huella' : 'assets/images/31_Huellas Escaneos24Pecaritajacusainopecaridecollar.webp'
+    },
+    {
+      'id' : 32,
+      'nombre' : 'Chancho de Monte',
+      'nombreCientifico' : 'Tayassu pecari',
+      'imagen' : 'assets/images/32_Tayassupecari.webp',
+      'huella' : 'assets/images/32_HuellasEscaneos34Tayassupecari.webp'
+    },
+    {
+      'id' : 33,
+      'nombre' : 'Cabro de Monte',
+      'nombreCientifico' : 'Odocoileus virginianus',
+      'imagen' : 'assets/images/33_Mazamaamerica.webp',
+      'huella' : 'assets/images/33_HuellasEscaneos18Mazamaamericana.webp'
+    },
+    {
+      'id' : 34,
+      'nombre' : 'Venado Cola Blanca',
+      'nombreCientifico' : 'Odocoileus virginianus',
+      'imagen' : 'assets/images/34_OdocoileusvirginianusTDLLisaSanchezAguilar.webp',
+      'huella' : 'assets/images/34_HuellasEscaneos22Odocoileusvirginianus-venadocolablanca.webp'
+    }
+  ];
+}
--- app_museo_nacional/android/app/src/main/kotlin/com/museonacional/app_museo_nacional/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.museonacional.app_museo_nacional
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
